852ee74198529d7658d940a55e4f780ac8b599e4
TKB Studios <11376202-TKB_Studios@users.noreply.gitlab.com>
2023-03-29 20:47:50 +0200

    bridge-new.py WORKS

--- sampleserver.py
+++ sampleserver.py
@@ -2,12 +2,13 @@ import socket
 
 # Set up the server socket
 host = '127.0.0.1'
-port = 5877
+port = 5556
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(1)
 
 print(f'Server listening on {host}:{port}')
+
 
 while True:
     # Wait for a client connection
@@ -16,6 +17,7 @@ while True:
 
     # Receive data from the client
     data = client_socket.recv(1024).decode().strip()
+    print(data)
 
     if data == 'Hello':
         # Send the response "OK" if the received data is "Hello"
