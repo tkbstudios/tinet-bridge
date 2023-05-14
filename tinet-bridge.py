8c5dca06d2d4942fce4e256c8769f3a8f11798b0
TKB Studios <69647028+tkbstudios@users.noreply.github.com>
2023-05-14 13:57:38 +0200

    Update TCP host removed unused code removed old update function

--- tinet-bridge.py
+++ tinet-bridge.py
@@ -7,13 +7,11 @@ import urllib.request
 import threading
 import math
 import os
-LATEST_VERSION_URL = "https://raw.githubusercontent.com/tkbstudios/ti84pluscenet-bridge/main/version.txt"
-CURRENT_VERSION = "0.0.1"
 
 #---BRIDGE CONFIG---#
 
-TCP_HOST = 'individual-builders.at.ply.gg' # TCP Server address. default: ti84pluscenethub.tkbstudios.tk
-TCP_PORT = 50252 # Server port. default: 50252 for default address
+TCP_HOST = 'tinethub.tkbstudios.tk' # TCP Server address. default: tinethub.tkbstudios.tk
+TCP_PORT = 5879 # Server port. default: 5879 for default address
 
 DEBUG = True # ENABLE DEBUG MODE, Shows more information in console, useful if staff asks for console log. default: True
 
@@ -24,45 +22,14 @@ EXIT_IF_PING_IS_ZERO = True # If the ping is 0, then disconnect the serial devic
 RETRY_DEFAULT_PORT_FOREVER = True # Retry the default rpi0W2 port (/dev/ttyACM0) forever if it fails. default: True
 
 #-END BRIDGE CONFIG-#
-
-
-# DO NOT TOUCH THIS CONF
-packets_dictionary = {
-    0x00: "USERNAME:",
-    0x01: "USERTOKEN:"
-}
-# STOP CONF
-
-serial_connection = serial.Serial()
-
-def checkForUpdate():
-    print("Checking for updates...")
-    try:
-        with urllib.request.urlopen(LATEST_VERSION_URL) as response:
-            latest_version = response.read().decode().strip()
-    except urllib.error.URLError as e: # type: ignore
-        print("Error: ", str(e.reason))
-        sys.exit(1)
-
-    if latest_version != CURRENT_VERSION:
-        print("New version available! Please update!")
-        print("https://github.com/tkbstudios/ti84pluscenet-bridge/blob/main/tinet-bridge.py")
-        print("Shortened link: https://tinyurl.com/mr3rxx3v")
-        sys.exit(1)
-    else:
-        print("Current version is up to date")
-
-
-def directUpdate():
+def updateBridge():
     print("Pulling latest files...")
     os.system("git config pull.ff only")
     os.system("git pull")
 
-#checkForUpdate()
-directUpdate()
+updateBridge()
 
 connected = False
-
 
 def CleanExit(serial_connection, server_client_sock, reason):
     print(str(reason))
@@ -74,7 +41,7 @@ def CleanExit(serial_connection, server_client_sock, reason):
     server_client_sock.close()
     sys.exit(0)
 
-
+'''
 def server_ping(server_client_sock, serial_connection):
     while True:
         start_time = time.time()
@@ -97,6 +64,7 @@ def server_ping(server_client_sock, serial_connection):
                 break
 
         time.sleep(PING_INTERVAL)
+'''
 
 def serial_read(serial_connection, server_client_sock):
     data = bytes()
@@ -197,14 +165,13 @@ time.sleep(1) # Add delay to prevent the client to not see the SERIAL_CONNECTED_
 print("\rReading data from serial device...                      ")
 try:
     # Windows doesn't allow connection even when allowed through private and public networks firewall
-    # Someone fix that lol
-    # deepcode ignore PythonSameEvalBinaryExpressiontrue: Nah just ignore it
+    '''
     if PING_SERVER == True:
         ping_server_thread = threading.Thread(target=server_ping(server_client_sock=server_client_sock, serial_connection=serial_connection))
         ping_server_thread.name = "ping_server"
         ping_server_thread.daemon = True
         ping_server_thread.start()
-
+    '''
     serial_read_thread = threading.Thread(target=serial_read, args=(serial_connection, server_client_sock))
     serial_read_thread.name = "SERIAL_READ_THREAD"
     serial_read_thread.start()
